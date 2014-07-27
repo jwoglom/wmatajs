2cf93de7159d94f31abd7d9b5278bfdebe40e727
James Woglom <j@wogloms.net>
2014-07-27 19:06:20 -0400

    combined files creation

---
+++ mkblob.php
@@ -0,0 +1,11 @@
+<?php
+$d = array();
+$d["lines"] = json_decode(file_get_contents("lines.txt"))->Lines;
+$d["stations"] = json_decode(file_get_contents("stations.txt"))->Stations;
+$ids = array("K08","D13","A15","B11","J03","G05","C15","E06","F11","E10","N06","G05");
+for($i=0; $i<sizeof($ids); $i++) {
+    $d["path-".$ids[$i]."-".$ids[$i+1]] = json_decode(file_get_contents("http://127.0.0.1/wmata/query.php?path=Rail.svc/json/jPath&api_key=***REMOVED***&format=json&FromStationCode=".$ids[$i]."&ToStationCode=".$ids[$i+1]))->Path;
+    $i++;
+}
+
+die(json_encode($d));
